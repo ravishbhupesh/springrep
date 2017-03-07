5668d97138519d244c77b8431e11bfa4679b9a1b
ravishb <bhupesh.ravish@rabobank.nl>
2017-03-07 11:47:29 +0100

    soap ws

---
+++ example.4.soapws/src/main/java/br/learn/springws/service/CountryEndpoint.java
@@ -0,0 +1,32 @@
+package br.learn.springws.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import br.learn.springws.model.CountryRepository;
+import br.learn.springws.schema.soapws.GetCountryRequest;
+import br.learn.springws.schema.soapws.GetCountryResponse;
+
+@Endpoint
+public class CountryEndpoint {
+	private static final String NAMESPACE_URI = "http://springws.learn.br/schema/soapws";
+
+	private CountryRepository countryRepository;
+
+	@Autowired
+	public CountryEndpoint(CountryRepository countryRepository) {
+		this.countryRepository = countryRepository;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+	@ResponsePayload
+	public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+		GetCountryResponse response = new GetCountryResponse();
+		response.setCountry(countryRepository.findCountry(request.getName()));
+
+		return response;
+	}
+}
