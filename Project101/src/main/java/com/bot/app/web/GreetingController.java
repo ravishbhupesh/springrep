f073e135f64c357227d1d41b2bb8350d8c84b669
ravishb <bhupesh.ravish@rabobank.nl>
2017-10-19 15:04:53 +0200

    ui changes

--- Project101/src/main/java/com/bot/app/web/GreetingController.java
+++ Project101/src/main/java/com/bot/app/web/GreetingController.java
@@ -11,7 +11,6 @@ public class GreetingController {
 	@RequestMapping("/greeting")
 	public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
 			Model model) {
-		System.out.println("GreetingController.greeting()");
 		model.addAttribute("name", name);
 		return "greeting";
 	}
--- Project101/src/main/resources/static/js/main.js
+++ Project101/src/main/resources/static/js/main.js
@@ -1,122 +1,34 @@
-$(document).ready(function () {
-
-    $("#search-form").submit(function (event) {
-
-        //stop submit the form, we will post it manually.
-        event.preventDefault();
-
-        fire_ajax_submit();
-
-    });
-
-});
-
-function fire_ajax_submit() {
-
-    var search = {}
-    search["username"] = $("#username").val();
-
-    $("#btn-search").prop("disabled", true);
-
-    $.ajax({
-        type: "POST",
-        contentType: "application/json",
-        url: "/api/search",
-        data: JSON.stringify(search),
-        dataType: 'json',
-        cache: false,
-        timeout: 600000,
-        success: function (data) {
-
-            var json = "<h4>Ajax Response</h4><pre>"
-                + JSON.stringify(data, null, 4) + "</pre>";
-            $('#feedback').html(json);
-
-            console.log("SUCCESS : ", data);
-            $("#btn-search").prop("disabled", false);
-
-        },
-        error: function (e) {
-
-            var json = "<h4>Ajax Response</h4><pre>"
-                + e.responseText + "</pre>";
-            $('#feedback').html(json);
-
-            console.log("ERROR : ", e);
-            $("#btn-search").prop("disabled", false);
-
-        }
-    });
-
-}
-
-$(document).ready(function(){
-    $('input[name="travelType"]').on('change', function() {
-        if($(this).attr('id') == 'id_travelType_0' && $(this).is(':checked')) {
-            $("#returnDate").attr('disabled', 'true');
-            $("#returnDate").val('')
-        } else {
-            $("#returnDate").removeAttr('disabled');
-        }
-    })
-})
-
-$(function() {
-    $("#id_depDate").datetimepicker({
-        format: "dd MM yyyy - hh:ii",
-        autoclose: true,
-        todayBtn: true,
-        startDate: "2013-02-14 10:00",
-        minuteStep: 10
-    });
-    
-    $("#id_returnDate").datetimepicker({
-        format: "dd MM yyyy - hh:ii",
-        autoclose: true,
-        todayBtn: true,
-        startDate: "2013-02-14 10:00",
-        minuteStep: 10
-    });
-    
-});
-
-function getFromDestination(elm) {
-    var url = "/get_destination/",
-        input = $(elm).children('input[name="fromDest"]'),
-        value = input.val();
-    $.ajax({
-        url: url,
-        type: 'GET',
-        data: {'q': value},
-        success: function(response){
-            response = JSON.parse(response)
-            console.log(response);
-            if (response.status) {
-                var available_dest = response.data;
-                $( "#id_fromDest" ).autocomplete({
-                    source: available_dest
-                });
-            }
-        }
-    })
-}
-function getToDestination(elm) {
-    var url = "/get_destination/",
-        input = $(elm).children('input[name="toDest"]'),
-        value = input.val();
-    $.ajax({
-        url: url,
-        type: 'GET',
-        data: {'q': value},
-        success: function(response){
-            response = JSON.parse(response)
-            console.log(response);
-            if (response.status) {
-                var available_dest = response.data;
-                $( "#id_toDest" ).autocomplete({
-                    source: available_dest
-                });
-            }
-        }
-    })
-}
+ $(function () {
+   var bindDatePicker = function() {
+		$(".date").datetimepicker({
+        format:'YYYY-MM-DD',
+		}).find('input:first').on("blur",function () {
+			// check if the date is correct. We can accept dd-mm-yyyy and yyyy-mm-dd.
+			// update the format if it's yyyy-mm-dd
+			var date = parseDate($(this).val());
+			if (! isValidDate(date)) {
+				//create date based on momentjs (we have that)
+				date = moment().format('YYYY-MM-DD');
+			}
+			$(this).val(date);
+		});
+	}
+   
+   var isValidDate = function(value, format) {
+		format = format || false;
+		// lets parse the date to the best of our knowledge
+		if (format) {
+			value = parseDate(value);
+		}
+		var timestamp = Date.parse(value);
+		return isNaN(timestamp) == false;
+   }
+   
+   var parseDate = function(value) {
+		var m = value.match(/^(\d{1,2})(\/|-)?(\d{1,2})(\/|-)?(\d{4})$/);
+		if (m)
+			value = m[5] + '-' + ("00" + m[3]).slice(-2) + '-' + ("00" + m[1]).slice(-2);
+		return value;
+   }
+   bindDatePicker();
+ });
